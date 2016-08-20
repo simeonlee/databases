c556450bf95316a5ae5d9f7b74a9a6a9ee4326c6
Simeon Lee <simeonhlee@gmail.com>
2016-08-20 15:57:26 -0700

    Move db index file to correct location and set up models

--- server/models/index.js
+++ server/models/index.js
@@ -1,84 +1,42 @@
-// var db = require('../db');
 var controller = require('../controllers/index');
-var dbConnection = require('../index').dbConnection;
-//dbConnection.connect();
+var db = require('../db/index');
+
 module.exports = {
   messages: {
-    get: function (callback) { // TODO: DO WE REMOVE REQ, RES?
-      dbConnection.query('SELECT * FROM messages', function(err, result) {
-        if (err) { throw err; }
-        callback(result);
-      });
-    }, // a function which produces all the messages
-    post: function (post, callback) {
-     // TODO: DO WE REMOVE REQ, RES?
-      console.log(post);
-      var username = post.username;
-      var roomname = post.roomname;
-      var user_id;
-      var room_id;
-      // var messageData = {'timestamp': new Date(), 'text': post.text, 'user_id': user_id, 'room_id': room_id};
-      //If User doesnt exist,Add user to Users table, immediately after get UserId, add to MessageData
-      //If Room exist, Add room to Rooms table, immediately after get roomID, add to MessageData
-      // dbConnection.query('INSERT INTO users SET ?', user, function(err, result) {
-      dbConnection.query('INSERT INTO users (name) VALUES (?) ON DUPLICATE KEY UPDATE', username, function(err, result) {
-        dbConnection.query('SELECT id FROM users WHERE name="' + username + '"', function(err, result) {
-          if (err) {
-            console.log(err);
-          }
-          console.log('waht is result??????', result);
-          user_id = result[0].id;
-          if (post.roomname) {
-            dbConnection.query('INSERT INTO rooms (name) VALUES (?) ON DUPLICATE KEY UPDATE', roomname, function(err, result) {
-              dbConnection.query('SELECT id FROM rooms WHERE name="' + roomname + '"', function(err, result) {
-                if (err) {
-                  console.log(err);
-                }
-                room_id = result[0].id;
-                if (post.text) {
-                  dbConnection.query('INSERT INTO messages (timestamp, text, user_id, room_id) VALUES (?, ?, ?, ?)', [new Date(), post.text, user_id, room_id], function(err, result) {
-                    if (err) {
-                      console.log(err);
-                    }
-                    callback();
-                  });
-                } else {
-                  callback();
-                }
-              });
-            });
-          } else {
-            callback();
-          }
-        });
-      });
-      //add message to Messages Table
-      // post = post || {timestamp: new Date(), text: 'Hello world! :)'};
-      
-      // Upon load of client, we'll do a post request with just the username to the user table
-      // That can be configured by doing a check if the post data has a message or not
-
-
-
-    } // a function which can be used to insert a message into the database
-  },
-
-  users: {
-    // Ditto as above.
     get: function (callback) {
-      dbConnection.query('SELECT * FROM users', function(err, result) {
+      var query = 'SELECT * FROM messages';
+      db.query(query, function(err, result) {
         if (err) { throw err; }
         callback(result);
       });
     },
-    post: function (user, callback) {
-      var userData = { name: user.username };
-      console.log('what is userdata ' + userData);
-      dbConnection.query('INSERT INTO users SET ?', userData, function(err, result) {
+    post: function (data, callback) {
+      var query = 'INSERT INTO messages (timestamp, text, user_id, room_id) \
+                   VALUES (?, ?, ?, ?)';
+      var params = [ new Data(), data.text, data.username, data.roomname ];
+      db.query(query, params, function(err, result) {
+        if (err) { throw err; }
+        callback(result);
+      });
+    }
+  },
+
+  users: {
+    get: function (callback) {
+      var query = 'SELECT * FROM users';
+      db.query(query, function(err, result) {
+        if (err) { throw err; }
+        callback(result);
+      });
+    },
+    post: function (data, callback) {
+      var query = 'INSERT INTO users (username) \
+                   VALUES (?)';
+      var params = [ data.username ];
+      db.query(query, params, function(err, result) {
         if (err) { throw err; }
         callback(result);
       });
     }
   }
 };
-
